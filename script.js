b1656ee1baef9aa7ba8ba090d747cf2718c81b27
Pomedor4ik <fpomedor4ik@gmail.com>
2023-03-28 18:40:21 +0300

    done

--- script.js
+++ script.js
@@ -1,51 +1,18 @@
-// let currentYear = new Date().getFullYear()
-// let birthYear = prompt("У якому році ви народились?")
-// let age = currentYear - birthYear
 
-// if (isNaN(birthYear)) {
-//     alert("Введіть будь ласка ціле числове значення")
-//     let birthYear = prompt("Уважно! У якому році ви народились?")
-//     alert(`Вам ${age} років!`)
-//     if (age > 100 ) {
-//         alert(`І Ви брехло !`)
-//     }
-    
-// }
-// else {
-//       alert(`Вам ${age} років!`)
-//       if (age > 100 ) {
-//           alert(`І Ви брехло !`)
-//       }
-//     }
 
-// let answer = prompt(`Вам зараз ${age} років так чи ні ?`)
-// if (answer.toLowerCase() == "так") {
-//   alert("Вірно!")
-// } else {
-//   alert("Неправильно!")
-// }
+let numbers = [4, 7, 2, 8, 4, 5];
+let sum = 0;
 
-let quantity = prompt("Введіть кількість чисел:")
-let i = 1
-let output = ""
-
-switch (true) {
-  case isNaN(quantity):
-    alert("Введіть будь ласка ціле числове значення")
-    break;
-  case quantity <= 0:
-    alert("Кількість має бути більше 0")
-    break;
-  default:
-    while (quantity > 0) {
-      if (i % 2 !== 0) {
-        output += i + " "
-        quantity--
-      }
-      i++
-    }
-    alert("Непарні числа: " + output)
-    break;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
 }
+let average = sum / numbers.length;
+console.log("Середнє значення масиву:", average);
 
-//quantity = количество
+let max = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    max = numbers[i];
+  }
+}
+console.log("Елемент з найбільшим числом:", max);
