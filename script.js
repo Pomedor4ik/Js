1211cd81f8458888053579047a17f847a9d8b1e9
Pomedor4ik <fpomedor4ik@gmail.com>
2023-04-17 20:44:45 +0300

    done

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-const orderForm = document.getElementById('orderForm');
-const productInput = document.getElementById('productInput');
-const quantityInput = document.getElementById('quantityInput');
-const orderList = document.getElementById('orderList');
-
-const orders = [];
-
-function renderOrders() {
-    orderList.innerHTML = '';
-    orders.forEach(order => {
-        const li = document.createElement('li');
-        li.textContent = `Товар: ${order.product}, Кількість: ${order.quantity}`;
-        orderList.appendChild(li);
-    });
+function changeTheme() {
+  var textElement = document.getElementById("myText");
+  if (textElement.style.color === "yellow") {
+    textElement.style.color = "blue";
+    textElement.style.backgroundColor = "yellow";
+  } else {
+    textElement.style.color = "yellow";
+    textElement.style.backgroundColor = "blue";
+  }
 }
 
-orderForm.addEventListener('submit', event => {
-    event.preventDefault();
-    const product = productInput.value;
-    const quantity = parseInt(quantityInput.value);
 
-    orders.push({product, quantity});
+// =======================================================================
 
-    productInput.value = '';
-    quantityInput.value = '';
 
-    renderOrders();
-}); 
+// let btnChenger = document.querySelector('#btnChenger');
+// let inputColors = document.querySelector('#inputColors');
+// let block = document.querySelector('.block');
 
+// inputColors.addEventListener("input", () => {
+//   block.style.background = inputColors.value
+// })
+
+
+// =======================================================================
+
+
+// const orderForm = document.getElementById('orderForm');
+// const productInput = document.getElementById('productInput');
+// const quantityInput = document.getElementById('quantityInput');
+// const orderList = document.getElementById('orderList');
+
+// orderList.style.border = "2px solid red";
+// orderList.style.width = "200px"
+// const orders = [];
+
+// function renderOrders() {
+//     orderList.innerHTML = '';
+//     orders.forEach(order => {
+//         const li = document.createElement('li');
+//         li.textContent = `Товар: ${order.product}, Кількість: ${order.quantity}`;
+//         orderList.appendChild(li);
+//     });
+// }
+
+// orderForm.addEventListener('submit', event => {
+//     event.preventDefault();
+//     const product = productInput.value;
+//     const quantity = parseInt(quantityInput.value);
+
+//     orders.push({product, quantity});
+
+//     productInput.value = '';
+//     quantityInput.value = '';
+
+//     renderOrders();
+// });
