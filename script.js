df256c12da81bbf36280d455115b6541d8306aa7
Pomedor4ik <fpomedor4ik@gmail.com>
2023-04-20 18:19:26 +0300

    done

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
-function changeTheme() {
-  var textElement = document.getElementById("myText");
-  if (textElement.style.color === "yellow") {
-    textElement.style.color = "blue";
-    textElement.style.backgroundColor = "yellow";
-  } else {
-    textElement.style.color = "yellow";
-    textElement.style.backgroundColor = "blue";
-  }
+let updateTable = () => {
+    let timeNow = getTime();
+    document.getElementById('hoursCell').innerText = timeNow.hours;
+    document.getElementById('minutesCell').innerText = timeNow.minutes;
+    document.getElementById('secondsCell').innerText = timeNow.seconds;
 }
 
+// Функція для форматування часу
+let formatTime = (timeObj) => {
+    if (timeObj.hours < 10) { 
+        timeObj.hours = `0${timeObj.hours}`;
+    }
+    if (timeObj.minutes < 10) { 
+        timeObj.minutes = `0${timeObj.minutes}`;
+    }
+    if (timeObj.seconds < 10) { 
+        timeObj.seconds = `0${timeObj.seconds}`;
+    }
+    return timeObj;
+}
 
-// =======================================================================
+// Функція для отримання поточного часу
+let getTime = () => {
+    let timeNow = new Date();
+    timeNow = {
+        hours: timeNow.getHours(),
+        minutes: timeNow.getMinutes(),
+        seconds: timeNow.getSeconds()
+    };
+    return formatTime(timeNow);
+}
 
+// Оновлення таблиці з інтервалом 1 секунда
+setInterval(updateTable, 1000);
 
-// let btnChenger = document.querySelector('#btnChenger');
-// let inputColors = document.querySelector('#inputColors');
-// let block = document.querySelector('.block');
+// =========================================
 
-// inputColors.addEventListener("input", () => {
-//   block.style.background = inputColors.value
-// })
-
-
-// =======================================================================
-
-
-// const orderForm = document.getElementById('orderForm');
-// const productInput = document.getElementById('productInput');
-// const quantityInput = document.getElementById('quantityInput');
-// const orderList = document.getElementById('orderList');
-
-// orderList.style.border = "2px solid red";
-// orderList.style.width = "200px"
-// const orders = [];
-
-// function renderOrders() {
-//     orderList.innerHTML = '';
-//     orders.forEach(order => {
-//         const li = document.createElement('li');
-//         li.textContent = `Товар: ${order.product}, Кількість: ${order.quantity}`;
-//         orderList.appendChild(li);
-//     });
-// }
-
-// orderForm.addEventListener('submit', event => {
-//     event.preventDefault();
-//     const product = productInput.value;
-//     const quantity = parseInt(quantityInput.value);
-
-//     orders.push({product, quantity});
-
-//     productInput.value = '';
-//     quantityInput.value = '';
-
-//     renderOrders();
-// });
+const changeTheme = () => {
+    let timeElement = document.getElementById("myTime");
+    if (timeElement.style.color == "grey") {
+      timeElement.style.color = "white";
+      timeElement.style.backgroundColor = "grey";
+    } else {
+      timeElement.style.color = "grey";
+      timeElement.style.backgroundColor = "black";
+    }
+  }
